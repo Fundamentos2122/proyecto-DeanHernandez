4b4368aaad88d057b8d7ea8992cc38c9631fcd77
DeanHernandez <89037186+DeanHernandez@users.noreply.github.com>
2022-04-06 16:00:06 -0500

    Add files via upload
    #7th upload

---
+++ Configuration.js
@@ -0,0 +1,9 @@
+document.getElementById("cc").addEventListener("click", AbrirCc);
+function AbrirCc() {
+  window.location.href="Cambiar_Contraseña.html";
+}
+
+document.getElementById("cn").addEventListener("click", AbrirCn);
+function AbrirCn() {
+  window.location.href="Cambiar_Usuario.html";
+}
