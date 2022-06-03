f64b92fa779ac55ecd7aa9ce3fe2f153998703e4
DeanHernandez <89037186+DeanHernandez@users.noreply.github.com>
2022-06-03 17:25:37 -0500

    Delete Configuration.js

--- Configuration.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("cc").addEventListener("click", AbrirCc);
-function AbrirCc() {
-  window.location.href="Cambiar_Contraseña.html";
-}
-
-document.getElementById("cn").addEventListener("click", AbrirCn);
-function AbrirCn() {
-  window.location.href="Cambiar_Usuario.html";
-}
